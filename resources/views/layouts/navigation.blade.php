494500214884dd110048887ba05e742c0f6ad0d1
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2024-07-12 21:51:09 +0700

    add mahasiswa

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -32,6 +32,11 @@
                         {{ __('Kelas') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('mahasiswa.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('Mahasiswa') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
