9490f381637a0bf0e1d5a9362fc8606dab5b22da
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2024-07-18 10:04:14 +0700

    add mahasiswa 2

--- database/migrations/2024_07_18_025401_create_mahasiswas_table.php
+++ database/migrations/2024_07_18_025401_create_mahasiswas_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('mahasiswas', function (Blueprint $table) {
             $table->id();
+            $table->integer('nim');
+            $table->string('nik');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jk');
+            $table->string('dusun');
+            $table->string('rtrw');
+            $table->string('kelurahan');
+            $table->string('kecamatan');
+            $table->string('kota');
+            $table->integer('kode_pos');
+            $table->string('no_hp');
+            $table->string('pendidikan_terakhir');
+            $table->string('asal_sekolah');
+            $table->string('jurusan_sekolah');
+            $table->integer('tahun_lulus');
+            $table->string('kode_kelas');
+            $table->integer('tahun_angkatan');
             $table->timestamps();
         });
     }
